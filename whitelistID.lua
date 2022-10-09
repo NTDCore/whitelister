6e2a4a23d0ef19a1cbf8851de40d70718c5c2e80
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-10-09 11:36:08 +0700

    Create whitelistID.lua

---
+++ whitelistID.lua
@@ -0,0 +1,21 @@
+local whitelistID = {
+   ["Monia"] = "2587041884",
+   ["MoniaMain"] = "2012715343"
+ }
+return whitelistID
+
+local Clients = {
+  ChatStrings1 = {
+		["KVOP25KYFPPP4"] = "vape",
+		["IO12GP56P4LGR"] = "future",
+		["RQYBPTYNURYZC"] = "rektsky",
+    ["N1GBTB3DYUKAC"] = "Nightbed"
+	},
+	ChatStrings2 = {
+		["vape"] = "KVOP25KYFPPP4",
+		["future"] = "IO12GP56P4LGR",
+		["rektsky"] = "RQYBPTYNURYZC",
+    ["Nightbed"] = "N1GBTB3DYUKAC"
+	},
+	ClientUsers = {}
+} -- fron vape lol
