15d08425d72f796a887edde3f0f6a3aa9c60af0f
Monia <102583640+NTDCore@users.noreply.github.com>
2023-05-24 19:00:09 +0700

    update Whitelists.lua

--- Whitelists.lua
+++ Whitelists.lua
@@ -1,22 +1,4 @@
 local whitelistID = {
-   ["Monia"] = "2587041884",
-   ["MoniaMain"] = "2012715343"
- }
+  "4589581089"
+}
 return whitelistID
---[[
-local Clients = {
-  ChatStrings1 = {
-		["KVOP25KYFPPP4"] = "vape",
-		["IO12GP56P4LGR"] = "future",
-		["RQYBPTYNURYZC"] = "rektsky",
-    ["N1GBTB3DYUKAC"] = "Nightbed"
-	},
-	ChatStrings2 = {
-		["vape"] = "KVOP25KYFPPP4",
-		["future"] = "IO12GP56P4LGR",
-		["rektsky"] = "RQYBPTYNURYZC",
-    ["Nightbed"] = "N1GBTB3DYUKAC"
-	},
-	ClientUsers = {}
-} -- fron vape lol
---]]
